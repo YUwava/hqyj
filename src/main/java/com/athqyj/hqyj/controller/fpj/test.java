8392105994d8a9e6339b87496d52373c791ad1d7
YUwava <2787107917@qq.com>
2020-04-25 23:30:30 +0800

    2020.4.25

--- src/main/java/com/athqyj/hqyj/controller/fpj/test.java
+++ src/main/java/com/athqyj/hqyj/controller/fpj/test.java
@@ -10,4 +10,42 @@ public class test {
     public String test(){
         return "home";
     }
+
+    @RequestMapping("welcome-1")
+    public String tests(){
+        return "welcome-1";
+    }
+
+    @RequestMapping("beforeCheck")
+    public String testss(){
+        return "beforeCheck";
+    }
+
+    @RequestMapping("ruleExpand")
+    public String testsss(){
+        return "ruleExpand";
+    }
+
+    @RequestMapping("xRayCheck")
+    public String testssss(){
+        return "xRayCheck";
+    }
+
+    @RequestMapping("oralCheck")
+    public String testsssss(){
+        return "oralCheck";
+    }
+
+    @RequestMapping("chemicalBear")
+    public String testssssss(){
+        return "chemicalBear";
+    }
+    @RequestMapping("tests")
+    public String testsssssss(){
+        return "chemicalBear";
+    }
+    @RequestMapping("checkBear")
+    public String testssssssss(){
+        return "checkBear";
+    }
 }
